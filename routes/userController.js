163143489828a53075f620aad9b9bdb562b20e1c
Zasz123 <ygm12345@naver.com>
2018-12-15 11:40:11 +0900

    파일 정리

--- routes/userController.js
+++ routes/userController.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 // 유저 라우터
 import userInsert from './userMiddleWares/signup/insert';
-import userShow from './userMiddleWares/myShow/show';
 import userLogin from './userMiddleWares/login/login';
 import userLogout from './userMiddleWares//logout/logout';
 
@@ -13,9 +12,6 @@ api.use('/', userInsert);
 
 // 유저 로그인 라우터
 api.use('/', userLogin);
-
-// 유저 정보보기 라우터
-api.use('/', userShow);
 
 // 유저 로그아웃 라우터
 api.use('/', userLogout);
